7bb438a4c0a1fde08344e3628e6c8ca147625d52
NGTri23 <gumit.ri23@gmail.com>
2025-04-13 16:08:45 +0200

    Update script.js
    création script

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+document.querySelectorAll('.block_text p').forEach(p => {
+  const imgElement = document.createElement('img');
+  imgElement.src = "https://ngtri23.github.io/images/logo_AWTD.png";
+  imgElement.style.width = "3vw";
+  imgElement.style.marginTop = "1vw";
+  imgElement.style.display = "block";
+  imgElement.style.margin = "1vw auto";
+  p.insertAdjacentElement('afterend', imgElement);
+});
