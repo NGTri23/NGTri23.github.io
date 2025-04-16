3183cb6cfc29dc4a33d04611bf59e15107e24a04
NGTri23 <gumit.ri23@gmail.com>
2025-04-16 23:16:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,49 @@
-document.querySelectorAll('.block_text p').forEach(p => {
+document.querySelectorAll('#block_text_règlement p').forEach(p => {
   const imgElement = document.createElement('img');
   imgElement.src = "https://ngtri23.github.io/images/logo_AWTD.png";
-  imgElement.style.width = "3vw";
-  imgElement.style.marginTop = "1vw";
+  imgElement.style.width = "7vh";
   imgElement.style.display = "block";
-  imgElement.style.margin = "1vw auto";
+  imgElement.style.margin = "3.5vw auto";
   p.insertAdjacentElement('afterend', imgElement);
+});
+
+document.querySelectorAll('.text_page p').forEach(p => {
+  const imgElement = document.createElement('img');
+  imgElement.src = "https://ngtri23.github.io/images/logo_AWTD.png";
+  imgElement.style.width = "5vh";
+  imgElement.style.display = "block";
+  imgElement.style.margin = "3.5vw auto";
+  p.insertAdjacentElement('afterend', imgElement);
+});
+
+
+const scrollContainer = document.querySelector('.photo-accueil');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+scrollContainer.addEventListener('mousedown', (e) => {
+  isDown = true;
+  scrollContainer.classList.add('active');
+  startX = e.pageX - scrollContainer.offsetLeft;
+  scrollLeft = scrollContainer.scrollLeft;
+  scrollContainer.style.cursor = 'grabbing';
+});
+
+scrollContainer.addEventListener('mouseleave', () => {
+  isDown = false;
+  scrollContainer.style.cursor = 'grab';
+});
+
+scrollContainer.addEventListener('mouseup', () => {
+  isDown = false;
+  scrollContainer.style.cursor = 'grab';
+});
+
+scrollContainer.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - scrollContainer.offsetLeft;
+  const walk = (x - startX) * 1; // sensibilité
+  scrollContainer.scrollLeft = scrollLeft - walk;
 });
